87a18ae862ff645c20f9db72d74c99db1b1a2452
welton <wangwei_omg@163.com>
2023-03-07 19:17:16 +0800

    fix message convert

--- src/main/java/com/tencent/wxcloudrun/config/RestTemplateConfig.java
+++ src/main/java/com/tencent/wxcloudrun/config/RestTemplateConfig.java
@@ -3,13 +3,29 @@ package com.tencent.wxcloudrun.config;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
     public RestTemplate build(RestTemplateBuilder restTemplateBuilder) {
-        return restTemplateBuilder.build();
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplateBuilder.additionalMessageConverters(new WxMappingJackson2HttpMessageConverter());
+        return restTemplate;
+    }
+
+    class WxMappingJackson2HttpMessageConverter extends MappingJackson2HttpMessageConverter {
+        public WxMappingJackson2HttpMessageConverter() {
+            List<MediaType> types = new ArrayList<>();
+            types.add(MediaType.TEXT_PLAIN);
+            setSupportedMediaTypes(types);
+        }
+
     }
 }
