5593de8d27731510a368486eae8e42f8d7fc35d0
welton <wangwei_omg@163.com>
2023-03-23 14:34:01 +0800

    update token

--- src/main/java/com/tencent/wxcloudrun/config/OpenAiServiceConfig.java
+++ src/main/java/com/tencent/wxcloudrun/config/OpenAiServiceConfig.java
@@ -1,6 +1,7 @@
 package com.tencent.wxcloudrun.config;
 
 import com.theokanning.openai.service.OpenAiService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,11 +9,11 @@ import org.springframework.context.annotation.Configuration;
 public class OpenAiServiceConfig {
 
 
+    @Value("${third.openai.token}")
+    private String token;
 
     @Bean
     public OpenAiService init() {
-
-        String token = System.getProperty("OPENAI_TOKEN");
 
         return new OpenAiService(token);
 
